c5cba57a7ba007470113293bd975b367561177da
Tomas Modesti <amodesti@fi.uba.ar>
2024-06-12 11:30:05 -0300

    Modificacio a main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #puse miles de import que me encontre, despues los podemos ir sacando
 from flask import Flask, request, jsonify
-from flask import flask,request
+from flask import request
 import datetime
 from flask_sqlalchemy import SQLAlchemy 
 from Modelos import app,Mineros, Minas, Minas_historial
 
 #aca pondriamos los endpoints
 
-app= flask(__name__)
+app= Flask(__name__)
 port = 5000
 
 @app.route('/',methods=['GET'])
