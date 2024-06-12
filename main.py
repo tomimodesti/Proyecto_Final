22680123daf5365c73d28d7c3530b2d704a71f9f
Tomas Modesti <amodesti@fi.uba.ar>
2024-06-12 11:11:38 -0300

    Modificacion a Main.py

--- main.py
+++ main.py
@@ -6,12 +6,44 @@ from flask import Flask, request, jsonify
 from flask import flask,request
 import datetime
 from flask_sqlalchemy import SQLAlchemy 
+from Modelos import app,Mineros, Minas, Minas_historial
 
 #aca pondriamos los endpoints
 
 app= flask(__name__)
 port = 5000
 
-@app.route('/')
-def home():
-    return "hello world"
+@app.route('/',methods=['GET'])
+def All_mineros():
+    try:
+        minero=Mineros.Query.all()
+        minero_data=[]
+        for Minero in minero:
+            Minero_data={
+                'Id':Minero.id,
+                'Nombre':Minero.nombre,
+                'Dinero':Minero.dinero,
+            }
+            minero_data.append(Minero_data)
+        return jsonify({'Mineros':minero_data})
+    except Exception as error:
+        print('Error al cargar datos',error)
+        return jsonify({'Mensaje: Error al cargar datos'})
+    
+@app.route('/minero/${Id}',methods=['GET'])
+def One_minero(id_minero):
+    try:  
+        # Tengo que ver como hacer la query de un minero especifico
+        minero=Mineros.Query.all()
+        minero_data=[]
+        for Minero in minero:
+            Minero_data={
+                'Id':Minero.id,
+                'Nombre':Minero.nombre,
+                'Dinero':Minero.dinero,
+            }
+            minero_data.append(Minero_data)
+        return jsonify({'Mineros':minero_data})
+    except Exception as error:
+        print('Error al cargar datos',error)
+        return jsonify({'Mensaje: Error al cargar datos'})
